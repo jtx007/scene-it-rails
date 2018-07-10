6783e2ba1624faa6990edac4afa9c35d9b0422e1
robertexe <robtabares92@gmail.com>
2018-07-10 12:04:55 -0400

    filled movie database with our API

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   has_many :posts
   has_many :users, through: :posts
 
+  def self.get_movies
+  	character_data = RestClient.get("https://api.themoviedb.org/4/list/8920?page=1&api_key=b833433d5a1c7f615ffcaa2c6259eccf")
+  	data = JSON.parse(character_data)
+  	movie_list = data['results'].each do |movie|
+  		mov = Movie.find_or_create_by(title: movie['title'], release_date: movie["release_date"], description: movie["description"], poster_path: movie["poster_path"], backdrop_path: movie["backdrop_path"])
+  	end
+    movie_list
+  end  
 end
