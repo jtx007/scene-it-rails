dd2c23fd2694273314d8d21522f3e68f35501e12
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-10 10:36:54 -0400

    ran migrations and seeded database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :watch_movies
   resources :movies
-  resources :watchlists
   resources :comments
   resources :posts
   resources :users
