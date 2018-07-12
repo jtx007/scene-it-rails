61e2f2e9a305a0bbe663a4f714b9676a7c6ed5cc
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-12 16:14:05 -0400

    created Edit button and added conditionals, have users movies show up on profile page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,16 @@ class PostsController < ApplicationController
     #byebug
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content, :movie_id)
